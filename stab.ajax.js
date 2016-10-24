ee4ee0ccdc942c08b425d83b2ea2ad654d362176
kongge <1216081546@qq.com>
2016-10-25 00:07:46 +0800

    ajax

---
+++ stab.ajax.js
@@ -0,0 +1,38 @@
+;stab.ajax = function(url, params, onSuccess, onError) {
+	var type = 'POST';
+
+  if(this.isFunction(params)){
+    onError = onSuccess
+    onSuccess = params
+    params = null
+    type = 'GET'
+  }
+
+  if(this.isObject(params)){
+  	var _p = [];
+  	for(var key in params){
+  		_p.push(key + '=' + params[key])
+  	}
+  	params = _p.join('&')
+  }
+
+	var xhr = new XMLHttpRequest()
+	xhr.open(type, url, true)
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8')
+
+	var abortTimeout = setTimeout(function() {
+      xhr.onreadystatechange = null
+      xhr.abort()
+      onError && onError.call(null, 'error: timeout.', xhr)
+  }, 12000)	// 12s
+
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304)) {
+			xhr.onreadystatechange = null
+			clearTimeout(abortTimeout)
+			onSuccess && onSuccess.call(null, xhr.responseText, xhr)
+		}
+	}
+
+	xhr.send(params);
+}
