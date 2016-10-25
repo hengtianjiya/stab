26e5cab2e59f128177717dc5f17f692166f251c6
kongge <1216081546@qq.com>
2016-10-25 16:49:25 +0800

    anime

---
+++ stab.anim.js
@@ -0,0 +1,24 @@
+;stab.cssPrefix = (function(_){
+  var body = _('body'), vendor = ['-webkit-', '-moz-', '-o-'];
+
+  for(var i = 0, len = vendor.length; i < len; i++){
+    if(!!body.css(vendor[i] + 'transition')) return vendor[i]
+  }
+
+  return '';
+})(stab)
+
+;stab.fn.anim = function(css, callback, duration, ease) {
+  duration = !duration ? 600 : (duration < 18 ? duration * 1000 : duration)
+  ease = ease || 'linear'
+
+  var _this = this,
+    prefix = stab.cssPrefix,
+    _css = prefix + 'transition:all ' + duration/1000 + 's ' + ease;
+
+  this.css(css + _css)
+  setTimeout(function(){
+    _this.css(prefix + 'transition:all 0s ease 0s;')
+    callback && callback.call(_this)
+  }, duration)
+}
